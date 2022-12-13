417fc1fe0b56fc9d370a115b987d2b197c31f6b0
leoAshu <ashutosh.ojha2009@gmail.com>
2022-12-13 13:45:50 -0800

    formats code

--- c/names.c
+++ c/names.c
@@ -1,6 +1,6 @@
-#include<cs50.h>
-#include<stdio.h>
-#include<string.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void) {
     string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
