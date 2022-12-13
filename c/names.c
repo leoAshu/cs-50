c0bd251f7c61204bb1627f2a4e0492c34e35e566
leoAshu <ashutosh.ojha2009@gmail.com>
2022-12-13 13:35:07 -0800

    adds string comparison code

---
+++ c/names.c
@@ -0,0 +1,17 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<string.h>
+
+int main(void) {
+    string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
+    int size = sizeof(names)/sizeof(names[0]);
+    
+    for(int i=0; i<size; i++) {
+        if(strcmp(names[i], "Ron")) {
+            printf("Found!\n");
+            return 0;
+        }
+    }
+    printf("Not Found!\n");
+    return 1;
+}
