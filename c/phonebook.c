c9a561a779617ed6bf763301bac734df30a546c5
leoAshu <ashutosh.ojha2009@gmail.com>
2022-12-13 13:55:01 -0800

    implements phonebook with struct definition

--- c/phonebook.c
+++ c/phonebook.c
@@ -2,13 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    string name;
+    string number;
+}
+person;
+
 int main(void) {
-    string names[] = {"Carter", "David"};
-    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
+    person people[2];
+
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
+
+    // string names[] = {"Carter", "David"};
+    // string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
 
     for(int i=0; i<2; i++) {
-        if(strcmp(names[i], "David") == 0) {
-            printf("Found %s\n", numbers[i]);
+        if(strcmp(people[i].name, "David") == 0) {
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
